c60dc4ec778ea671722cbd1ebb3ba66b1aed7a24
DongIkkk <w8h0412@gmail.com>
2022-10-28 10:31:33 +0900

    articles 생성,수정,삭제

--- articles/views.py
+++ articles/views.py
@@ -1,26 +1,49 @@
 from rest_framework.views import APIView
-from rest_framework import status
-from rest_framework import permissions
+from rest_framework import status, permissions
 from rest_framework.response import Response
-
+from articles.models import Article
+from articles.serializers import ArticleSerializer, ArticleListSerializer, ArticleCreateSerializer
+from rest_framework.generics import get_object_or_404
 # Create your views here.
 class ArticleView(APIView):
     def get(self, request):
-        pass
+        articles = Article.objects.all()
+        serializer = ArticleListSerializer(articles, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        pass
+        serializer = ArticleCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
 
 class ArticleDetailView(APIView):
     def get(self, request, article_id):
-        pass
+        article = Article.objects.get(id=article_id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, article_id):
-        pass
+        article = get_object_or_404(Article, id=article_id)
+        if request.user == article.user:
+            serializer = ArticleCreateSerializer(article, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
 
     def delete(self, request, article_id):
-        pass
+        article = get_object_or_404(Article, id=article_id)
+        if request.user == article.user:
+            article.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        
 
 
 class CommentView(APIView):
