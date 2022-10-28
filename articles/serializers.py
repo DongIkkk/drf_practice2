5da86f398a01bbf66f311e9be47956d724cd50fb
DongIkkk <w8h0412@gmail.com>
2022-10-28 11:32:57 +0900

    comment crud

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from articles.models import Article
+from articles.models import Article, Comment
 
 class ArticleSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
@@ -29,3 +29,13 @@ class ArticleListSerializer(serializers.ModelSerializer):
         model = Article
         fields = ("pk", "title", "image", "updated_at", "user")
 
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ("content",)
